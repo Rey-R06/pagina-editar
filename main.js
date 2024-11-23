f271320fa7b5f491df478a4bf3da72c190c4925c
Rey-R06 <165798210+Rey-R06@users.noreply.github.com>
2024-11-22 20:08:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-function cambio(n) {
-    main = document.getElementById("main")
-    header = document.getElementById("header")
-    footer = document.getElementById("footer")
-
-    switch (n) {
-        case 1:
-            header.style.backgroundColor = "#5bc0de";
-            main.style.backgroundColor = "#5bc0de";
-            footer.style.backgroundColor = "#5bc0de";   
-            break;
-        case 2:
-            header.style.backgroundColor = "#ffff00";
-            main.style.backgroundColor = "#ffff00";
-            footer.style.backgroundColor = "#ffff00";
-            break;
-        case 3:
-            header.style.backgroundColor = "#FF0000";
-            main.style.backgroundColor = "#FF0000";
-            footer.style.backgroundColor = "#FF0000";   
-            
-            break;
-        case 4:
-            header.style.backgroundColor = "#008f39";
-            main.style.backgroundColor = "#008f39"; 
-            footer.style.backgroundColor = "#008f39"; 
-            break;
-        default:
-            break;
-    }
-}
 
 let n = 0;
-menueditar = document.getElementById("edit")
 function menuedit() {
+let menueditar = document.getElementById("edit");
     if (n == 0) {
         menueditar.style.opacity = "1";
         menueditar.style.visibility = "visible";
@@ -42,5 +11,20 @@ function menuedit() {
         menueditar.style.visibility = "hidden";
         n = 0;
     }
-
 }
+
+const main = document.getElementById("main")
+const header = document.getElementById("header")
+const footer = document.getElementById("footer")
+const body = document.getElementById('body');
+
+selector.addEventListener('input', (event) => {
+    header.style.backgroundColor = event.target.value; // Cambia el fondo
+    main.style.backgroundColor = event.target.value; // Cambia el fondo
+    footer.style.backgroundColor = event.target.value; // Cambia el fondo
+});
+
+
+selector2.addEventListener('input', (event) => {
+    body.style.backgroundColor = event.target.value; // Cambia el fondo
+});
